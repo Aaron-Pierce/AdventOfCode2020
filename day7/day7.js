4264e6c2efe316d5ce1b593086206f70c0e0b42f
Aaron <aaronmp01@gmail.com>
2020-12-07 08:31:34 -0600

    commented day 7

--- day7/day7.js
+++ day7/day7.js
@@ -12,10 +12,15 @@ function addToAdjacencyMap(map, key, value){
 }
 
 function part1(){
+    //like an adjacency matrix but easier
     let adjacencyMap = {
         "shiny gold": new Set()
     };
 
+    //The map represents a directed graph.
+    //Every bag points to the bags that contain it.
+    //i.e. every child bag points to its parents.
+    //this loop constructs the directed graph
     for(let line of input){
         let split = line.split(" bags contain ");
         let bagColor = split[0];
@@ -27,12 +32,15 @@ function part1(){
         }
     }
 
+    //if we start at shiny gold and dfs (or bfs or however you want to search)
+    //Every unique bag we visit is a new bag that could possibly contain a gold bag
+    //because we arrived at that bag by traversing through parent bags who contained a gold bag.
+    //this will do a dfs and every time we visit a new node increment the counter
     let visitedCount = 0;
     function traverse(alreadyVisited, nodeName){
         if(alreadyVisited.has(nodeName)) return;
         visitedCount++;
         alreadyVisited.add(nodeName);
-        console.log(nodeName)
         if(adjacencyMap[nodeName]){
             adjacencyMap[nodeName].forEach(child => {
                 traverse(alreadyVisited, child);
@@ -41,9 +49,11 @@ function part1(){
     }
 
     traverse(new Set(), "shiny gold")
-    console.log(visitedCount - 1)  //shiny gold bags cannot themselves carry a shiny gold bag.
+    console.log(visitedCount - 1)
+    //shiny gold bags cannot themselves carry a shiny gold bag.
+    //the loop counts a shiny gold bag as a unique bag that is in the search, so it must contain a gold bag, 
+    ///but it can't carry itself so we subtract one to fix that.
 }
-
 part1();
 
 class Bag{
@@ -56,6 +66,16 @@ class Bag{
 function part2(){
     let map = {};
 
+    //creates another graph of the opposite direction
+    //every parent points to all of their children
+    //each child is an instance of Bag, which contains the color
+    //of the child bag and the number of that bag that is in the parent
+    //so that you can multiply the number of its children by that value.
+    //For example, if a red bag contains 3 yellow bags, we want to multiply
+    //the number of bags inside the yellow bag by 3 to get the 
+    //total number of children in red.
+    
+    //this loop constructs that graph
     for(let line of input){
         let split = line.split(" bags contain ");
         let bagColor = split[0];
@@ -64,20 +84,23 @@ function part2(){
             const bagStrSplit = bagString.split(" ");
             let subBagAmount = parseInt(bagStrSplit)
             let subBagColor = bagStrSplit[1] + " " + bagStrSplit[2];
+            //"wavy blue bag contains no other bags"
+            //has color of "other bags."
+            //so this is a hacky way to not include parents with no children.
             if(subBagColor == "other bags.") continue;
             map[bagColor].push(new Bag(subBagColor, subBagAmount));
         }
     }
-    console.log(map)
 
+    //every bag's value is the sum of the values of its children
+    //So a bag that contains 4 red and 2 yellow has a value of 6 bags + 4(children in red) + 2(children in yellow)
+    //this recursively computes that
     function traverse(bagName, bagAmount){
         let numberOfChildren = 0;
         if(map[bagName]){
             map[bagName].forEach(child => {
                 numberOfChildren += child.amount*bagAmount;
-                console.log(child.color)
                 let traversedValue = bagAmount * traverse(child.color, child.amount);
-                console.log(traversedValue)
                 numberOfChildren += traversedValue;
             });
             return numberOfChildren;
